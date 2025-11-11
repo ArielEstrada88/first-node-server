0bb8a7a826fd32bf12d56b21267769460ba8eb93
ArielEstrada88 <aest4983@gmail.com>
2025-11-10 17:04:15 -0700

    Changed proper response to root EP

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 // Root Endpoint(HOME Endpoint)
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Goofy Rooster!");
 });
 //Route
  
